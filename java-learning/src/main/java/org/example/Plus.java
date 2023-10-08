bbf170e0038475151506d63d6dd09a8ec8dd5a59
liangxiaojun <fatalxiao@163.com>
2023-10-08 15:24:48 +0800

    update

---
+++ java-learning/src/main/java/org/example/Plus.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Plus {
+    public static void main(String[] args) {
+
+        int a, b;
+        a = 1;
+        b = 2;
+
+        System.out.printf("%d + %d = %d%n", a, b, a + b);
+
+    }
+}
