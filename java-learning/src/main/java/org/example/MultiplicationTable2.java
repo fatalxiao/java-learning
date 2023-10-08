da3324c2e5c6345f1828d6653446968d17274205
liangxiaojun <fatalxiao@163.com>
2023-10-08 16:19:32 +0800

    update

--- java-learning/src/main/java/org/example/MultiplicationTable2.java
+++ java-learning/src/main/java/org/example/MultiplicationTable2.java
@@ -4,10 +4,10 @@ public class MultiplicationTable2 {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <= 9; j++) {
-                int a = j;
-                int b = j + i - 1;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                int a = j + 1;
+                int b = j + i + 1;
                 if (b <= 9) {
                     System.out.printf("%d x %d = %d\t", a, b, a * b);
                 }
