6b1d98a1a76b7d7669cbab7be0279edfb70094df
liangxiaojun <fatalxiao@163.com>
2023-10-08 15:50:04 +0800

    update

---
+++ java-learning/src/main/java/org/example/MultiplesOfFour.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class MultiplesOfFour {
+
+    public static void main(String[] args) {
+
+        for (int i = 4; i <= 9; i++) {
+            System.out.printf("4 x %d = %d\n", i, 4 * i);
+        }
+
+    }
+
+}
