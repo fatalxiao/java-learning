1d10ecc21d6a57383e8dbdf2488a69fdc525dbcb
liangxiaojun <fatalxiao@163.com>
2023-10-08 15:25:53 +0800

    update

---
+++ java-learning/src/main/java/org/example/Times.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Times {
+
+    public static void main(String[] args) {
+
+        int a, b;
+        a = 4;
+        b = 5;
+
+        System.out.printf("%d x %d = %d%n", a, b, a * b);
+
+    }
+
+}
