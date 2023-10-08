41a2d9b59030f7f63651c0295e9c474a0ee0df48
liangxiaojun <fatalxiao@163.com>
2023-10-08 15:56:43 +0800

    update

---
+++ java-learning/src/main/java/org/example/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                if (i >= j) {
+                    System.out.printf("%d x %d = %d\t", j, i, j * i);
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+}
